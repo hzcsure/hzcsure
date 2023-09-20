5994908eb9040f2813db28d5bea6d02f813d1bda
hzcsure <59165420+hzcsure@users.noreply.github.com>
2023-09-20 21:20:00 +0800

    Update test.py

--- test.py
+++ test.py
@@ -5,5 +5,10 @@ from telethon import TelegramClient, events, sync
 from telethon import functions, types
 from telethon.sessions import StringSession
 
-print("hello telethon！！！")
-print(os.environ.get("ARG_A", ""))
+api_id = os.environ.get("API_ID", "")
+api_hash = os.environ.get("API_HASH", "")
+session_string = os.environ.get("SESSION_STRING", "")
+
+client = TelegramClient(StringSession(session_string), api_id, api_hash)
+client.start()
+print(client.get_me().stringify())
