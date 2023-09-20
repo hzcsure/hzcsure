0896aaefc0ac006e280bb699d7430c4ef85a911d
hzcsure <59165420+hzcsure@users.noreply.github.com>
2023-09-20 17:22:09 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
+import os
 from telethon import TelegramClient, events, sync
 from telethon import functions, types
 from telethon.sessions import StringSession
 
 print("hello telethon！！！")
- print(ARG_A is {os.environ.get("ARG_A", "")}')
+print(ARG_A is {os.environ.get("ARG_A", "")}')
