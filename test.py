530bb673e991733ac7fa1831e7499674e8039427
hzcsure <59165420+hzcsure@users.noreply.github.com>
2023-09-20 14:02:59 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+from telethon import TelegramClient, events, sync
+from telethon import functions, types
+from telethon.sessions import StringSession
+
+print("hello telethon！！！")
