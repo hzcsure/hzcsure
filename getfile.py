84f8f803e00fa9a5ce0bf6e3b0c8f01a0f2b941b
hzcsure <59165420+hzcsure@users.noreply.github.com>
2026-03-24 16:49:07 +0800

    Update getfile.py

--- getfile.py
+++ getfile.py
@@ -10,16 +10,5 @@ import time
 api_id = os.environ.get("API_ID", "")
 api_hash = os.environ.get("API_HASH", "")
 session_string = os.environ.get("SESSION_STRING", "")
-async def get_channel_id(channel_username):
-    async with TelegramClient(session_string, api_id, api_hash) as client:
-        # 支持 @xxx、t.me/xxx、频道名
-        entity = await client.get_entity(channel_username)
-        # 频道ID = -100 + 纯数字ID
-        channel_id = entity.id
-        full_id = f"-100{channel_id}"
-        print(f"频道ID: {full_id}")
-        return full_id
-
-# 替换成你的频道
-import asyncio
-asyncio.run(get_channel_id("@SCHPD_SUB"))
+client = TelegramClient(StringSession(session_string), api_id, api_hash)
+client.start()
