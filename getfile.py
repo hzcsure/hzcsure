c9cd3f13e661b48f8853539a8c79a7cf8174c0d0
hzcsure <59165420+hzcsure@users.noreply.github.com>
2026-03-24 16:58:20 +0800

    Update getfile.py

--- getfile.py
+++ getfile.py
@@ -11,4 +11,11 @@ api_id = os.environ.get("API_ID", "")
 api_hash = os.environ.get("API_HASH", "")
 session_string = os.environ.get("SESSION_STRING", "")
 client = TelegramClient(StringSession(session_string), api_id, api_hash)
-client.start()
+CHANNEL = "@SCHPD_SUB"
+
+async def main():
+    async with TelegramClient(StringSession(session_string), api_id, api_hash) as client:
+        chat = await client.get_entity(CHANNEL)
+        print(f"频道ID: -100{chat.id}")
+
+asyncio.run(main())
